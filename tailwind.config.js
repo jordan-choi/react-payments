067e9847c492b646b0f970250aa9e635b37bd9bf
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-01-31 23:04:31 +0900

    chore: Add base styles and theme.colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   darkMode: false,
   content: [],
   theme: {
+    colors: {
+      white: '#fff',
+      gray0: '#ecebf1',
+      gray1: '#e5e5e5',
+      gray2: '#d3d3d3',
+      gray3: '#9ca3af',
+      gray4: '#575757',
+      gray5: '#525252',
+      gray6: '#383838',
+      card: '#94dacd',
+      cardchip: '#cbba64',
+      shadow: 'rgba(0, 0, 0, 0.25)',
+    },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        fontSize: '10px',
+      });
+    }),
+  ],
+};

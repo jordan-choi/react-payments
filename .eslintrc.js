5bc712e46af9cd9b7f78aeda6359f29e3bb07428
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-01-31 12:14:32 +0900

    chore: Install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ["plugin:@typescript-eslint/eslint-recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "prettier", "plugin:storybook/recommended"],
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-}
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};

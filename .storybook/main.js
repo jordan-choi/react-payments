6334d6dcb0503d9f1194df648141b2f228c5448a
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-01-31 14:06:08 +0900

    chore: fix storybook-emotion-11-error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,22 @@
+const path = require('path');
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      },
+    },
+  }),
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};

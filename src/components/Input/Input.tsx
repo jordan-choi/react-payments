9550c7f3d2d49e5bd683deae7b54251edbdce2d3
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-02-01 21:51:38 +0900

    feat: Add ref to Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,9 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { forwardRef, InputHTMLAttributes } from 'react';
 import tw from 'twin.macro';
 import styled from '@emotion/styled/macro';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  underline?: boolean;
+  underline: boolean;
 }
 
 const InputBasic = styled.input([
@@ -14,11 +14,19 @@ const InputUnderline = styled.input([
   tw`text-center border-none bg-none outline-none mx-0 my-6.4 px-0 py-1.6 border-b border-b-gray6`,
 ]);
 
-export default function Input({
-  underline = false,
-  ...props
-}: InputProps): JSX.Element {
-  return (
-    <>{underline ? <InputUnderline {...props} /> : <InputBasic {...props} />}</>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ underline = false, ...props }: InputProps, ref): JSX.Element => {
+    return (
+      <>
+        {underline ? (
+          <InputUnderline ref={ref} {...props} />
+        ) : (
+          <InputBasic ref={ref} {...props} />
+        )}
+      </>
+    );
+  }
+);
+Input.displayName = 'Input';
+
+export default Input;

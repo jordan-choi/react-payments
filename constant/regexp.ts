c0d2175644abad4a2311738b2212f7d8132059f7
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-02-02 21:36:17 +0900

    feat: Add MONTH regex. Add maxlength condition to NAME regex

--- constant/regexp.ts
+++ constant/regexp.ts
@@ -1,7 +1,8 @@
 const REGEXP = {
   ONLYNUMBER: /^[0-9]/,
-  USERNAME: /^(?=.*[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9])(?=.{6,})/,
-  PASSWORD: /[0-9]{4,}/,
+  NAME: /^(?=.*[ㄱ-ㅎ|가-힣|a-z|A-Z])(?=.{,30})/,
+  PASSWORD: /^[0-9]{4,}/,
+  MONTH: /(0[1-9]|1[0-2])/,
 };
 
 export default REGEXP;

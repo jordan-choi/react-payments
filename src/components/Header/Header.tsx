df65e73b59e0c324b64d1012ca3fd8b46c7f0d4d
ipf-yychoi <yychoi@iportfoilo.co.kr>
2022-02-03 09:32:40 +0900

    feat: Export PageTItle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 }
 
 const HeaderWrapper = styled.div([tw`w-full flex items-center`]);
-const PageTitle = styled.h2([
+export const PageTitle = styled.h2([
   tw`font-medium text-8 leading-[2.2rem] flex items-center text-gray6`,
 ]);
 
